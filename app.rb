c98c65c6cbc0463f2d6efd0c350e1d7c1871f6c8
emanon001 <emanon001@gmail.com>
2015-05-08 13:18:52 +0900

    Logger を使用してエラー情報を出力するよう修正

--- app.rb
+++ app.rb
@@ -1,12 +1,15 @@
 require './init'
+require 'logger'
 
 interval = (ENV['SYNC_INTERVAL'] || 60).to_i
+STDOUT.sync = true
+logger = Logger.new(STDOUT)
 
 loop do
   begin
     Elbvss.client.sync
   rescue => e
-    p e
+    logger.error e
   ensure
     sleep interval
   end
